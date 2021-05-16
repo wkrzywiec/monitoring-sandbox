e952a06018431f4889c87fe242e67b75620acab9
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2021-05-16 16:02:53 +0200

    simple java app added

---
+++ app/src/main/java/io/wkrzywiec/blog/jvm/performance/app/AppApplication.java
@@ -0,0 +1,13 @@
+package io.wkrzywiec.blog.jvm.performance.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/io/wkrzywiec/blog/jvm/performance/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wkrzywiec.blog.jvm.performance.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
