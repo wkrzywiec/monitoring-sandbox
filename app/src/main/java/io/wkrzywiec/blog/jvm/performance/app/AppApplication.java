d7a8bb1dc78ce679214575d8ff86273700dd8f34
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2021-09-28 11:34:57 +0200

    heavy task scheduler

---
+++ app/src/main/java/io/wkrzywiec/blog/jvm/performance/app/AppApplication.java
@@ -0,0 +1,14 @@
+package io.wkrzywiec.blog.jvm.performance.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+}
